315defb05a7321b502e5fdc4ef4685b243fba5c1
Crowdbotics <team@crowdbotics.com>
2020-05-21 19:10:25 +0000

    Update models

--- new_test_11_dev_4690/wsgi.py
+++ new_test_11_dev_4690/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_test_11_dev_4690.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "new_test_11_dev_4690.settings")
 
 application = get_wsgi_application()
